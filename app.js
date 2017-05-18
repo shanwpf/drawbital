68e81645f628e2fde463cbe0928f11deeefdad69
Shannon Wong <shanwpf@gmail.com>
2017-05-18 11:37:20 +0800

    Update server config

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ app.get('/', function (req, res) {
 app.use('/client', express.static(__dirname + '/client'));
 
 // Start server
-server.listen(2000);
-console.log("Server started");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 io.sockets.on('connection', function (socket) {
     console.log('socket connection');
