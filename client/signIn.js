78e6e8c522e9673a59b4dec5ac9380364662ab1f
koo1993 <chinchyekoo@hotmail.com>
2017-06-04 10:39:22 +0800

    prevent user from spamming log in and cause multiple same user instances on the server

--- client/signIn.js
+++ client/signIn.js
@@ -4,32 +4,47 @@ var signInDivSignIn = document.getElementById('signInDiv-signIn');
 var signInDivSignUp = document.getElementById('signInDiv-signUp');
 var signInDivPassword = document.getElementById('signInDiv-password');
 
+function disableButtons(bool)
+{
+    signInDivSignIn.disabled = bool;
+    signInDivSignUp.disabled = bool;
+}
+
 signInDivSignIn.onclick = function () {
     if (signInDivUsername.value.trim() === "" || signInDivPassword.value.trim() === "" )
         alert("dont it blank :(");
     else
-    socket.emit('signIn', { username: signInDivUsername.value.trim(), password: signInDivPassword.value });
+    {
+        disableButtons(true);
+        socket.emit('signIn', { username: signInDivUsername.value.trim(), password: signInDivPassword.value });
+    }
 }
 
 signInDivSignUp.onclick = function () {
     if (signInDivUsername.value.trim() === "" || signInDivPassword.value.trim() === "" )
         alert("dont it blank :(");
-    else
+    else{
+        disableButtons(true);
         socket.emit('signUp', { username: signInDivUsername.value.trim(), password: signInDivPassword.value });
+    }
 }
 
 socket.on('signInResponse', function (data) {
     if (data.success) {
         signInDiv.style.display = 'none';
         displayDiv.style.display = 'inline-block';
-    } else
+    } else{
+        disableButtons(false);
         alert("Sign in unsuccessful.");
+    }
 });
 
 
 socket.on('signUpResponse', function (data) {
     if (data.success) {
         alert("Sign up successful.");
-    } else
+    } else{
+        disableButtons(false);
         alert("Sign up unsuccessful, Username taken");
+    }
 });
