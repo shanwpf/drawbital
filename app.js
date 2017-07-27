shanwpf/drawbital

commit 21777ae5949a2c1e5df45e956c87187b75ae24bf

User is now informed of inablitiy to load during Guessketch

app.js
@@ -796,12 +796,14 @@ class Client {
     }
 
     load(idx) {
+        if(this.room.game)
+            return false;
+
          var self = this;
          loadSaveData(this.name, idx, function (res) {
             if (res) {
-                if(!self.room.game)
-                    self.room.surface.loadState(res);
-                }
+                self.room.surface.loadState(res);
+            }
         });
     }
 
@@ -932,8 +934,11 @@ class Client {
             fn({nameExists: false});
             client.newSave(data.saveName);
         })
-        socket.on('loadState', function (data) {
-            client.load(data.idx);
+        socket.on('loadState', function (data, fn) {
+            if(client.load(data.idx))
+                fn(true);
+            else
+                fn(false);
         })
         socket.on('getSaveList', function() {
             socket.emit('saveList', { saves: client.saves });

client/draw.js
@@ -475,9 +475,13 @@ socket.on('saveList', data => {
 })
 
 $('#saveList').on('dblclick', '.list-group-item', function() {
-    socket.emit('loadState', { idx: this.getAttribute("saveName") })
+    socket.emit('loadState', { idx: this.getAttribute("saveName") }, function(data) {
+        if(data) 
+            showSnackBar('Save loaded')
+        else
+            showSnackBar('Load not allowed in Guessketch')
+    })
     $('#loadModal').modal('hide');
-    showSnackBar('Save loaded')
 })
 
 $('#saveList').on('click', '.close', function() {

commit f619db40f7e8017ddb8807b2d29fd380e1482fb9

Fix bug introduced by 21777ae Method should return true at the end

app.js
@@ -805,6 +805,7 @@ class Client {
                 self.room.surface.loadState(res);
             }
         });
+        return true;
     }
 
     deleteSave(idx) {